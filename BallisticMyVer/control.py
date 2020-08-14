78af646d0413647200da2fca5aa6d8644c3ab049
Kai Lawrence <kai@kais-mbp.home>
2020-08-14 22:29:29 +0100

    debugged and applied graphic output

--- BallisticMyVer/control.py
+++ BallisticMyVer/control.py
@@ -18,15 +18,17 @@ import matplotlib.pyplot as plt
 POPULATION_INITIAL_SIZE = 200
 POPULATION_LIMIT = 10000
 
-NUM_EPOCH = 25000
+# NUM_EPOCH = 25000
+NUM_EPOCH = 10
 BATCH_SIZE = 20000
 
-NB_RETRAIN = 5
-NB_QD_ITERATIONS = 10000
+NB_RETRAIN = 1#5
+# NB_QD_ITERATIONS = 10000
+NB_QD_ITERATIONS = 10
 
 MUTATION_RATE = 0.1
 ETA = 10
-EPS = 0.1
+EPSILON = 0.1
 
 K_NEAREST_NEIGHBOURS = 15
 INITIAL_NOVLETY = 0.01
@@ -53,13 +55,17 @@ def train_ae(autoencoder, population):
     loss_plot = []
     gpu_options = tf.GPUOptions(allow_growth=True)
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,log_device_placement=False)) as session:
+        init_all_vars_op = tf.variables_initializer(tf.global_variables(), name='init_all_vars_op')
+        session.run(init_all_vars_op) 
+        autoencoder.saver.restore(session, "MY_MODEL")
+
         # Make batches
         batch_list = make_batches(population)
         print("Beginning Training of Autoencoder")
         loss = 0
         for epoch in range(NUM_EPOCH):
             if epoch % 250 == 0:
-                print("At training epoch " + str(epoch) + ", we're " + str(epoch/NUM_EPOCH * 100) + "%% of the way there!")
+                print("At training epoch " + str(epoch) + ", we're " + str(epoch/NUM_EPOCH * 100) + "% of the way there!")
             for batch in batch_list:
                 for member in batch:
                     image = member.get_traj_image()
@@ -67,10 +73,15 @@ def train_ae(autoencoder, population):
                     # autoencoder.step()
             avg_loss = np.mean(loss)
             loss_plot.append(avg_loss)
+
+        # Save the current autoencoder
+        autoencoder.saver.save(session, "MY_MODEL")
+
     print("Training Complete")
     plt.plot(loss_plot)
     plt.xlabel("Epoch")
     plt.ylabel("Reconstruction Loss")
+    plt.title("Training Loss")
     plt.show()
     return autoencoder
 
@@ -79,23 +90,26 @@ def prep_apply(data):
     return scaled_data
 
 def calculate_novelty_threshold(latent_space):
-    X = np.array(latent_space)
 
-    XX = []
-    for row in latent_space:
+    rows = len(latent_space)
+    cols = len(latent_space[0][0])
+    X = np.zeros((rows, cols))
+    for i in range(rows):
+        for j in range(cols):
+            X[i][j] = latent_space[i][0][j]
+
+    XX = np.zeros((rows, 1))
+
+    for i in range(rows):
         sigma = 0
-        for el in row:
-            sigma += el**2
-        XX.append(sigma)
-    XX = np.array(XX)
+        for j in range(cols):
+            sigma += X[i][j]**2
+        XX[i][0] = sigma
 
     XY = (2*X) @ X.T
 
-    dims = XX.shape
-    print(dims)
-
-    dist = XX @ np.ones(1, dims[0])
-    dist += np.ones(dims[0], 1) @ XX.T
+    dist = XX @ np.ones((1, rows))
+    dist += np.ones((rows, 1)) @ XX.T
     dist -= XY
 
     maxdist = np.sqrt(np.max(dist))
@@ -108,23 +122,39 @@ def calculate_novelty_threshold(latent_space):
 
 
 def mut_eval(indiv_params):
+    # Implements polynomial mutation
     new_indiv = individual.indiv()
-    new_indiv.eval(indiv_params)
+    new_params = indiv_params.copy()
+    for p in range(len(indiv_params)):
+        mutate = random.uniform(0, 1)
+        if mutate < MUTATION_RATE:
+            u = random.uniform(0, 1)
+            if u <= 0.5:
+                # Calculate delta_L
+                delta_L = (2 * u)**(1 / (1 + ETA)) - 1
+                new_params[p] = indiv_params[p] + delta_L * (indiv_params[p] - FIT_MIN)
+            else:
+                # Calculate delta_R
+                delta_R = 1 - (2 * ( (1-u))**(1/ (1 + ETA) ) )
+                new_params[p] = indiv_params[p] + delta_R * (FIT_MAX - indiv_params[p])
+
+    new_indiv.eval(new_params)
     return new_indiv
 
 def does_dominate(curr_threshold, k_n_n, dist_from_k_n_n, population):
     dominated_indiv = -1
+    x_1_novelty = 0
     # If novelty threshold is greater than the nearest neighbour but less than the second nearest neighbour
     if (curr_threshold > dist_from_k_n_n[0]) and (curr_threshold < dist_from_k_n_n[1]):
 
-        pop_without_x_2 = population
+        pop_without_x_2 = population.copy()
         del pop_without_x_2[k_n_n[0]]
         x_1_novelty, _, _ = dist_from_k_n_n[1]
         x_2_novelty, _, _ = calculate_novelty(population[k_n_n[0]].get_bd(), pop_without_x_2, curr_threshold, False)
 
-        # Find if exclusive eta dominance is met according to Cully QD Framework
+        # Find if exclusive epsilon dominance is met according to Cully QD Framework
         # First Condition
-        if x_1_novelty >= (1 - ETA) * x_2_novelty:
+        if x_1_novelty >= (1 - EPSILON) * x_2_novelty:
             # The Second Condition measures Quality, i.e. a fitness function
             # In AURORA this does not exist as having such a defeats the purpose of autonomous discovery
             # I have included because why the hell not
@@ -149,7 +179,7 @@ def calculate_novelty(this_bd, population, curr_threshold, check_dominate):
     # If the population is still too small
     if len(population) < K_NEAREST_NEIGHBOURS:
         novelty = 99999
-        return novelty
+        return novelty, -1
     else:
         k_n_n = []
         dist_from_k_n_n = []
@@ -169,6 +199,9 @@ def calculate_novelty(this_bd, population, curr_threshold, check_dominate):
                     k_n_n[-1] = member
 
         # Sort lists so that minimum novelty is at the start
+        if len(dist_from_k_n_n) != K_NEAREST_NEIGHBOURS:
+            print("X_1_NOVELTY ERROR")
+            print(dist_from_k_n_n)
         k_n_n = [x for _,x in sorted(zip(dist_from_k_n_n, k_n_n))]
         dist_from_k_n_n.sort()
         novelty = dist_from_k_n_n[0]
@@ -179,6 +212,20 @@ def calculate_novelty(this_bd, population, curr_threshold, check_dominate):
             dominated_indiv, novelty = does_dominate(curr_threshold, k_n_n, dist_from_k_n_n, population)
 
         return novelty, dominated_indiv
+
+def plot_latent(population):
+    x = []
+    y = []
+    for member in population:
+        this_x, this_y = member.get_bd()[0]
+        x.append(this_x)
+        y.append(this_y)
+    
+    plt.scatter(x, y, c='b')
+    plt.xlabel("Encoded dimension 1")
+    plt.ylabel("Encoded dimension 2")
+    plt.title("Latent Space")
+    plt.show()
 
 def AURORA_ballistic_task():
     # Randomly generate some controllers
@@ -202,10 +249,12 @@ def AURORA_ballistic_task():
     my_ae = AE()
 
     # Initialize variables
-    gpu_options = tf.GPUOptions(allow_growth=True)
-    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,log_device_placement=False)) as session:
-        init_all_vars_op = tf.variables_initializer(tf.global_variables(), name='init_all_vars_op')
-        session.run(init_all_vars_op) 
+    # gpu_options = tf.GPUOptions(allow_growth=True)
+    # with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options,log_device_placement=False)) as session:
+    #     init_all_vars_op = tf.variables_initializer(tf.global_variables(), name='init_all_vars_op')
+    #     session.run(init_all_vars_op) 
+        # tf.train.write_graph(session.graph_def, '../resources', 'graph.pb', as_text=False)
+        # save_path_init = my_ae.saver.save(session, "../resources/model_init.ckpt")
 
     # Train the dimension reduction algorithm (the Autoencoder) on the dataset
     my_ae = train_ae(my_ae, pop)
@@ -215,6 +264,7 @@ def AURORA_ballistic_task():
 
     # Use the now trained Autoencoder to get the behavioural descriptors
     with tf.Session() as sess:
+        my_ae.saver.restore(sess, "MY_MODEL")
         for member in pop:
             image = member.get_traj_image()
             # Sensory data is then projected into the latent space, this is used as the behavioural descriptor
@@ -222,21 +272,23 @@ def AURORA_ballistic_task():
             member.set_bd(member_bd)
             latent_space.append(member_bd)
     
-   
+
     # Randomly intialize the QD algorithm
     # Calculate starting novelty threshold
     threshold = INITIAL_NOVLETY
 
-    # Main algorithm loop
-    with tf.Session() as sess:
+    # Main algorithm
 
-        # Loop for controlling number of times Autoencoder is retrained
-        for i in range(NB_RETRAIN):
-            # Begin Quality Diversity iterations
-            print("Beginning QD iterations")
+    # Loop for controlling number of times Autoencoder is retrained
+    for i in range(NB_RETRAIN):
+        # Begin Quality Diversity iterations
+        print("Beginning QD iterations")
+
+        with tf.Session() as sess:
+            my_ae.saver.restore(sess, "MY_MODEL")
             for j in range(NB_QD_ITERATIONS):
                 if j%100 == 0:
-                    print("At QD iteration" + str(j) + ", we're " + str(j/NB_QD_ITERATIONS * 100) + "%% of the way there!")
+                    print("At QD iteration " + str(j) + ", we're " + str(j/NB_QD_ITERATIONS * 100) + "% of the way there!")
 
                 # 1. Randomly select a controller from the population
                 this_indiv = random.choice(pop)
@@ -262,45 +314,47 @@ def AURORA_ballistic_task():
                     new_indiv.set_bd(new_bd)
                     new_indiv.set_novelty(novelty)
                     pop[dominated] = new_indiv
-            print("Finished QD iterations")
+        print("Finished QD iterations")
 
-            # 6. Retrain Autoencoder after a number of QD iterations
-            print("Calling Autoencoder retrain")
-            my_ae = train_ae(my_ae, pop)
-            print("Completed retraining")
+        # 6. Retrain Autoencoder after a number of QD iterations
+        print("Calling Autoencoder retrain")
+        my_ae = train_ae(my_ae, pop)
+        print("Completed retraining")
 
-            # 7. Clear latent space
-            latent_space = []
+        # 7. Clear latent space
+        latent_space = []
 
-            # 8. Assign the members of the population the new Behavioural Descriptors
-            #    and refill the latent space
+        # 8. Assign the members of the population the new Behavioural Descriptors
+        #    and refill the latent space
+        with tf.Session() as sess:
+            my_ae.saver.restore(sess, "MY_MODEL")
             for member in pop:
                 image = member.get_traj_image()
                 member_bd = sess.run(my_ae.latent, feed_dict={my_ae.x : image, my_ae.keep_prob : 0, my_ae.global_step : 25000})
                 member.set_bd(member_bd)
                 latent_space.append(member_bd)
 
-            # 9. Calculate new novelty threshold to ensure population size less than 10000
-            threshold = calculate_novelty_threshold(latent_space)
-            print("New novelty threshold is " + str(threshold))
+        # 9. Calculate new novelty threshold to ensure population size less than 10000
+        threshold = calculate_novelty_threshold(latent_space)
+        print("New novelty threshold is " + str(threshold))
 
-            # 10. Update population so that only members with novel bds are allowed
-            print("Add viable members back to population")
-            new_pop = []
-            for member in pop:
-                this_bd = member.get_bd()
-                novelty, dominated = calculate_novelty(this_bd, new_pop, threshold, True)
-                if dominated == -1:                           #    If the individual did not dominate another individual
-                    if novelty >= threshold:                  #    If the individual is novel
-                        new_indiv.set_bd(member)
-                        member.set_novelty(novelty)
-                        new_pop.append(member)
-                else:                                         #    If the individual dominated another individual
-                    member.set_bd(new_bd)
+        # 10. Update population so that only members with novel bds are allowed
+        print("Add viable members back to population")
+        latent_space = []
+        new_pop = []
+        for member in pop:
+            this_bd = member.get_bd()
+            novelty, dominated = calculate_novelty(this_bd, new_pop, threshold, True)
+            if dominated == -1:                           #    If the individual did not dominate another individual
+                if novelty >= threshold:                  #    If the individual is novel
                     member.set_novelty(novelty)
-                    new_pop[dominated] = member
+                    new_pop.append(member)
+            else:                                         #    If the individual dominated another individual
+                member.set_novelty(novelty)
+                new_pop[dominated] = member
 
-            pop = new_pop
+        pop = new_pop
+        plot_latent(pop)
 
 
 
